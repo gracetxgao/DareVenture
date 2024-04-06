eca31fdac1129982fee3ab4b5f78457252198395
Doris Zhang <doriszhang912@gmail.com>
2024-04-06 16:32:26 -0700

    Started login styles

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,24 +1,26 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
 import { Input } from 'react-native-elements';
 
 const LoginScreen = ({ navigation }) => {
     return (
         <View>
-            <Text>login</Text>
+            <Text style={styles.text}>Log In</Text>
             <Input
-                placeholder='email'
+                placeholder='Email'
             />
             <Input
-                placeholder='password'
+                placeholder='Password'
             />
             <Button
+                buttonStyle ={styles.button}
                 title="login"
                 onPress={() =>
                     navigation.navigate('Home')
                 }
             />
             <Button
+                buttonStyle ={styles.button}
                 title="create an account"
                 onPress={() =>
                     navigation.navigate('Signup')
@@ -27,5 +29,20 @@ const LoginScreen = ({ navigation }) => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 45,
+        alignContent: "center",
+        justifyContent: 'center',
+    },
+    button: {
+        backgroundColor: '#5DB075',
+    }
+
+
+
+});
+
 
 export default LoginScreen;
