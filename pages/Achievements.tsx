54e20bc4a8be06e07f9a2c2766117274867490d6
Doris Zhang <doriszhang912@gmail.com>
2024-04-06 20:28:22 -0700

    fixed spacing

--- pages/Achievements.tsx
+++ pages/Achievements.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
         fontSize: 35,
         color:"#fff",
         fontWeight: 'bold',
-        paddingTop: 10,
+        paddingTop: 25,
     },
     textContainer: {
         width: 250,
