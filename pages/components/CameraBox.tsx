d2d6bbbb203a0931a5d46943ad72dea593a0405c
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-04-06 22:03:23 -0700

    made camera work

--- pages/components/CameraBox.tsx
+++ pages/components/CameraBox.tsx
@@ -1,35 +1,37 @@
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { Button } from 'react-native-elements';
+import vgcService from '../../server/ventures'
+import { useState } from 'react';
 
 const CameraBox = () => {
 
+    const [imageUri, setImageUri] = useState(null);
+
     const openCamera = async () => {
-        let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+        const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
         if (permissionResult.granted === false) {
           alert('Permission to access camera is required!');
           return;
         }
-        let pickerResult = await ImagePicker.launchCameraAsync();
-        if (pickerResult.canceled === true) {
-          return;
-        }
-    
-        // Handle the image picked from the camera here
-      };
+        const result = await ImagePicker.launchCameraAsync();
 
+        console.log(`result is ${result}`);
+        
+        if (result.canceled === false) {
+            console.log(result.assets[0].uri);
+            setImageUri(result.assets[0].uri);
+        }
+    }
       
     return (
-        <View  style={styles.container}>
+        <View style={styles.container}>
             <TouchableOpacity onPress={openCamera}>
                 <Image 
                     source={{uri: 'https://t4.ftcdn.net/jpg/01/07/57/91/360_F_107579101_QVlTG43Fwg9Q6ggwF436MPIBTVpaKKtb.jpg'}}
                     style={styles.image} />
                 <Text>attach photo</Text>
             </TouchableOpacity>
-            
         </View>
-        
     )
 }
 
@@ -50,4 +52,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default CameraBox
+export default CameraBox;
