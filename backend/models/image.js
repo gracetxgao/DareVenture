69bbcaaf3a915a960afb305f2707cc8adf854679
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-04-07 00:26:23 -0700

    more backend

--- backend/models/image.js
+++ backend/models/image.js
@@ -13,17 +13,13 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
-const personSchema = new mongoose.Schema({
-  name: {
+const imageSchema = new mongoose.Schema({
+  uri: {
     type: String,
-    minLength: 3,
-    required: true,
   },
-  number: Number,
-  id: String
 })
 
-personSchema.set('toJSON', {
+imageSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -31,4 +27,4 @@ personSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Image', imageSchema)
---
+++ backend/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://gracetxgao:${password}@cluster0.syarjah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const imageSchema = new mongoose.Schema({
+  uri: String,
+})
+
+const Image = mongoose.model('image', imageSchema)
+
+if (process.argv.length === 3) { // node mongo.js password
+  console.log('images:');
+  Image
+    .find({})
+    .then(result => {
+      result.forEach(image => {
+        const uri = image.uri
+        console.log(uri);
+      })
+      mongoose.connection.close()
+    })
+} else if (process.argv.length === 4) { // node mongo.js password uri
+    const uri = process.argv[3]
+    const image = new Image({
+      uri: uri,
+    })
+    image.save().then(result => {
+      console.log(`added ${uri} to images`);
+      mongoose.connection.close()
+    })
+} else {
+  console.log('unknown input');
+  process.exit(1)
+}
